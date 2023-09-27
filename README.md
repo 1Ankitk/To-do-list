670d270becbc965b69c487232b302119fb6fafd3
User <ankitkumar101967@gmail.com>
2023-09-27 12:22:12 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+#React Firebase Authentication User Based To do List 
+
+## Features of App
+ * User can register.
+ * User can login.
+ * User can add their own to do list once login.
+ * Every user has their own dashboard
+ * User can delete and add wishlist
+        
+##Instruction
+  * Update your firebase config in Firebase.js
+  * Open Terminal in root directory
+  * Set up : `yarn install`
+  * Start Project: `npm start`  
+  * Deploy : `npm run build`
+  * 
+        
+    
---
+++ src/Firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDbcK-BOEPSbL8k6IzgwyXx5-9ufVSo1G0",
+  authDomain: "todolist-84b5d.firebaseapp.com",
+  databaseURL: "https://todolist-84b5d-default-rtdb.firebaseio.com",
+  projectId: "todolist-84b5d",
+  storageBucket: "todolist-84b5d.appspot.com",
+  messagingSenderId: "859787408932",
+  appId: "1:859787408932:web:2c1d6771fda8d3181e0ab1",
+  measurementId: "G-66B69HB1V9"
+};
+export const firebaseApp = firebase.initializeApp(config);
+export const users = firebaseApp.database().ref().child('users');
+export const usersWishlist = firebaseApp.database().ref().child('usersWishlist');
