807cb13e4a5df4cc12eb533592a786a8a125f652
1Ankitk <110518334+1Ankitk@users.noreply.github.com>
2023-10-09 11:13:05 +0530

    first commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,9 +5,9 @@ var config = {
     apiKey: "YOUR_API_KEY",
   authDomain: "YOUR_AUTH_DOMAIN",
   databaseURL: "https://your-project-name.firebaseio.com",   // Replace with your actual URL
-  projectId: "todolist-84b5d",
-  storageBucket: "todolist-84b5d.appspot.com",
-  messagingSenderId: "859787408932",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
   appId: "1:859787408932:web:2c1d6771fda8d3181e0ab1",
 };
 export const firebaseApp = firebase.initializeApp(config);
